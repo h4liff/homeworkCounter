418351dc1d4b80d1874c79027589ea43480e10b0
h4liff <h4liff@yandex.ru>
2023-10-22 22:40:24 +0500

    Домашняя работа, удалены лишние комментарии, переменная count перенесена в тело класса, расставлены модификаторы доступа private для @IBoutlet

--- Counter homework/ViewController.swift
+++ Counter homework/ViewController.swift
@@ -1,19 +1,54 @@
-//
-//  ViewController.swift
-//  Counter homework
-//
-//  Created by Nikolay Krivtsov on 19.10.2023.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    @IBOutlet private weak var lableCounter: UILabel!
+    @IBOutlet private weak var textChanges: UITextView!
+    @IBOutlet private weak var buttonReset: UIButton!
+    @IBOutlet private weak var buttonMinus: UIButton!
+    @IBOutlet private weak var buttonPlus: UIButton!
+    
+    private var count: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textChanges.isEditable = false
     }
-
-
+    
+    private func addToChanges(_ message: String) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        let dateStr = dateFormatter.string(from: Date())
+        let newText = "\n[\(dateStr)]: \(message)"
+        textChanges.text += newText
+        scrollToBottom()
+    }
+    
+    private func scrollToBottom() {
+        let bottom = NSMakeRange(textChanges.text.count - 1, 1)
+        textChanges.scrollRangeToVisible(bottom)
+    }
+    
+    @IBAction private func buttonResetAction(_ sender: Any) {
+        count = 0
+        lableCounter.text = "Значение счетчика: \(count)"
+        addToChanges("значение сброшено")
+    }
+    
+    @IBAction private func buttonMinusAction(_ sender: Any) {
+        if count > 0 {
+            count -= 1
+            addToChanges("значение изменено на -1")
+            lableCounter.text = "Значение счетчика: \(count)"
+        } else {
+            addToChanges("попытка уменьшить значение счётчика ниже 0")
+            lableCounter.text = "Значение счетчика: \(count)"
+        }
+    }
+    
+    @IBAction private func buttonPlusAction(_ sender: Any) {
+        count += 1
+        addToChanges("значение изменено на +1")
+        lableCounter.text = "Значение счетчика: \(count)"
+    }
 }
 
